d2ccff8e8c4b47c72c8079f7aabd607bf5428eb5
mrugala <jedrzej.mrugala@gmail.com>
2017-09-04 21:53:19 +0200

    I don't know what I committed last time... This time really added LcdManager variadic input. Compiles, because I didn't start writing anything in LcdManager.cpp and commented out the #include directive.

--- LcdManager.h
+++ LcdManager.h
@@ -1,5 +1,29 @@
 #pragma once
 
+#include <Arduino.h>
+
+template<typename T>
+String to_string(T val)
+{
+    return String(val);
+}
+template<>
+String to_string(double val)
+{
+    String ret;
+    if (abs(val) < 10.) ret = String(val, 4);
+    else if (abs(val) < 100.) ret = String(val, 3);
+    else if (abs(val) < 1000.) ret = String(val, 2);
+    else if (abs(val) < 10000.) ret = String(val, 1);
+    else ret = String(val, 0);
+    return ret;
+}
+template<>
+String to_string(int val)
+{
+    return String(val, DEC);
+}
+
 class LcdManager
 {
 public:
@@ -13,20 +37,34 @@ public:
         const unsigned d7;
     };
 
-    LcdManager(const LcdPins pins);
-    virtual ~LcdManager();
+    //LcdManager(const LcdPins pins);
+    //virtual ~LcdManager();
 
-    int init(const unsigned cols, const unsigned rows, const unsigned screens);
+    //int init(const unsigned cols, const unsigned rows, const unsigned screens);
     
     template<typename... Args>
-    void print(const unsigned screen, Args... args)
+    void print(const unsigned screen, const unsigned row, Args... args)
     {
-         
+         display_content[screen][row] = getString(args...);
     }
     void goToScreen(const unsigned screen);
 
 private:
+    template<typename T, typename... Args>
+    String getString(T val, Args... args)
+    {
+        return to_string(val) + getString(args...);
+    }
 
+    String getString()
+    {
+        return String("");
+    }
+
+    String** display_content;
+    const unsigned cols;
+    const unsigned rows;
+    const unsigned screens;
 };
 
 
