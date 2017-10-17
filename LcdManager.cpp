edaa4d5b1512b1c910c23f7be4cb088c6b27ab11
Jędrzej Mrugała <jedrzej.mrugala@gmail.com>
2017-10-17 20:09:05 +0200

    Extracted LcdManager's implementation to a PrintManager template class to be able to support different devices with the same functionality and enable easier testing without using actual LCD screen (i.e. using COM port instead).

--- LcdManager.cpp
+++ LcdManager.cpp
@@ -2,81 +2,8 @@
 #include <LiquidCrystal.h>
 #include "project_defines.h"
 
-#define MAX_LINE_LENGTH     0xFF
-#define MAX_NUMBER_OF_LINES 0xF
-
 LcdManager::LcdManager(const LcdPins pins)
     : LcdManager(new LiquidCrystal(pins.rs, pins.enable, pins.d4, pins.d5, pins.d6, pins.d7))
 {
-}
-
-LcdManager::LcdManager(LiquidCrystal* lcd) : lcd(lcd)
-{
-    current_position.screen = 0;
-    current_position.row = 0;
-    current_position.col = 0;
-}
-
-LcdManager::~LcdManager()
-{
-    if (was_initialized)
-    {
-        for (unsigned screen = 0; screen < screens; ++screen)
-            delete[] display_content[screen];
-        delete[] display_content;
-    }
-
-    delete lcd;
-}
-
-int LcdManager::init(const unsigned cols, const unsigned rows, const unsigned screens)
-{
-    this->cols = cols;
-    this->rows = rows;
-    this->screens = screens;
-
-    display_content = new String*[screens];
-    for (unsigned screen = 0; screen < screens; ++screen)
-        display_content[screen] = new String[MAX_NUMBER_OF_LINES];
-
-    was_initialized = true;
-    lcd->begin(cols, rows);
-    goToScreen(0);
-
-    return 0;
-}
-
-void LcdManager::goToScreen(const unsigned screen)
-{
-    if (current_position.screen != screen)
-    {
-        current_position.screen = screen;
-        printImpl(screen);
-    }
-}
-
-void LcdManager::printImpl(const unsigned screen)
-{
-    for (unsigned row = current_position.row; row < current_position.row + this->rows; row++)
-    {
-        unsigned display_line = 0;
-        lcd->setCursor(0, display_line);
-        lcd->print(getContent(screen, row, 0));
-    }
-}
-
-String LcdManager::getContent(const unsigned screen, const unsigned row, const unsigned col)
-{
-    auto line_length = display_content[screen][row].length();
-    String ret;
-    if (line_length >= this->cols)
-        ret = display_content[screen][row].substring(col, col+MAX_LINE_LENGTH);
-    else
-    {
-        ret = display_content[screen][row];
-        for (auto col = line_length; col < MAX_LINE_LENGTH; ++col)
-            ret = ret + " ";
-    }
-    return ret;
 }
 
--- LcdManager.h
+++ LcdManager.h
@@ -2,10 +2,11 @@
 
 #include <Arduino.h>
 #include "string_operations.h"
+#include "PrintManager.h"
 
 class LiquidCrystal;
 
-class LcdManager
+class LcdManager : public PrintManager<LiquidCrystal>
 {
 public:
     struct LcdPins
@@ -18,41 +19,9 @@ public:
         const unsigned d7;
     };
 
+    using PrintManager<LiquidCrystal>::PrintManager;
     LcdManager(const LcdPins pins);
-    LcdManager(LiquidCrystal* lcd);
-    virtual ~LcdManager();
-
-    int init(const unsigned cols, const unsigned rows, const unsigned screens);
-    
-    template<typename... Args>
-    void print(const unsigned screen, const unsigned row, Args... args)
-    {
-        this->display_content[screen][row] = StringOps::getString(args...);
-        if (current_position.screen == screen)
-        {
-            this->printImpl(screen);
-        }
-    }
-    void goToScreen(const unsigned screen);
-
-private:
-    String** display_content;
-    unsigned cols;
-    unsigned rows;
-    unsigned screens;
-
-    struct { 
-        unsigned col;
-        unsigned row;
-        unsigned screen;
-    } current_position;
-
-    bool was_initialized {false};
-
-    LiquidCrystal* lcd;
-
-    void printImpl(const unsigned screen);
-    String getContent(const unsigned screen, const unsigned row, const unsigned col);
+    virtual ~LcdManager() = default;
 };
 
 
